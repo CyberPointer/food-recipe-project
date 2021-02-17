6e852432640cb8fa2f327415325d26984f8bd1b7
bob <test@example.com>
2021-02-17 07:10:45 +0100

    added notes, recipes, ingredients and unit of mesure.

---
+++ src/main/java/com/project/recipe/FoodRecipeProjectApplication.java
@@ -0,0 +1,13 @@
+package com.project.recipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodRecipeProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodRecipeProjectApplication.class, args);
+	}
+
+}
